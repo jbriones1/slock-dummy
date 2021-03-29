67181890eaa66d9d4bbe5f878892327891b45483
Phil <philfeiteng@gmail.com>
2021-03-29 11:26:23 -0700

    modified static files

---
+++ scripts/room.js
@@ -0,0 +1,11 @@
+const buttonClick1 = e => {
+	document.getElementById("result1").innerHTML =
+		e.target.innerText + " was clicked";
+};
+
+const buttonClick2 = e => {
+	document.getElementById("result2").innerHTML =
+		e.target.innerText + " was clicked";
+};
+
+

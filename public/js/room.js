60d798226843ee70a42d166ce06645e094150f49
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-01 14:37:13 -0700

    Changed some functions when pushing to the client

--- public/js/room.js
+++ public/js/room.js
@@ -1,26 +1,28 @@
-const socket = new Socket('ws://localhost:4000/socket', {params: {token: "token", vsn: "2.0.0"}});
+const socket = new Socket('ws://localhost:4000/socket', {params: {vsn: "2.0.0"}});
 socket.connect();
 console.log(socket);
+const roomId = window.location.pathname[window.location.pathname.length - 1];
 
-let channel = socket.channel('chat:general', {});
+let channel = socket.channel(`chat:client${roomId}`, {});
 channel.join()
 .receive('ok', resp => console.log('ok'))
 .receive('err', resp => console.log('error'));
 
 // Set the name of the room
-const roomId = window.location.pathname[window.location.pathname.length - 1];
 document.getElementById('roomId').innerText = roomId;
 
 const buttonClick1 = e => {
 	document.getElementById("result1").innerHTML =
 		e.target.innerText + " was clicked";
   
-  channel.push('shout', {name: 'Dummy client', body: 'From dummy client'});
+  channel.push('shout', {name: `Client ${roomId} - Player 1`, body: e.target.innerText});
 };
 
 const buttonClick2 = e => {
 	document.getElementById("result2").innerHTML =
 		e.target.innerText + " was clicked";
+  
+  channel.push('shout', {name: `Client ${roomId} - Player 2`, body: e.target.innerText});
 };
 
 

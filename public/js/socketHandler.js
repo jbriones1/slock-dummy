8edc39758ed9411ce159e7a207dfbc39158517f2
Jon Andre Briones <jbriones1@my.bcit.ca>
2021-04-03 14:11:40 -0700

    Removed unnecessary code

--- public/js/socketHandler.js
+++ public/js/socketHandler.js
@@ -1,13 +1,1 @@
-// const socket = new Socket('wss://jondreb-multi-chat.herokuapp.com/socket', {params: {vsn: "2.0.0"}});
-// socket.connect();
-
-// let channel = socket.channel('chat:general', {});
-// channel.join()
-// .receive('ok', resp => console.log('ok'))
-// .receive('err', resp => console.log('error'));
-
-// export const makeSocket = (url, options = {}) => {
-//   return new Socket(url, options);
-// };
-
 export const socket = new Socket('ws://localhost:4000/socket', {});
